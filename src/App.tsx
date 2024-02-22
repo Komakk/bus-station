83a05bc5d5c170c8848f54a02d64f3983e24bd4d
Anton Mikhailov <komakkomakk@gmail.com>
2024-02-22 06:27:07 +0400

    add widget

--- src/App.tsx
+++ src/App.tsx
@@ -137,20 +137,51 @@ export default function App() {
         </section>
         <aside className=" px-4 pt-6">
           <button className=" py-3 mb-5 w-full block border border-blue-800 text-blue-700 ">
+            <img
+              className=" inline-block mr-1"
+              src="./public/jbility-toggle.svg"
+            />
             Accessibility options
           </button>
           <button className=" py-3 mb-5 w-full block border border-blue-800 text-blue-700">
+            <img
+              className=" w-5 inline-block mr-1"
+              src="./public/contacts.svg"
+            />
             Client support centre
           </button>
           <button className=" py-3 mb-5 w-full block border border-blue-800 text-blue-700">
+            <img className=" w-6 inline-block mr-1" src="./public/story.svg" />
             Feedbacks
           </button>
           <h2 className=" pb-4 text-xl text-blue-700">News</h2>
           <div className=" mb-4   shadow-lg text-gray-800">
-            <p className=" p-3 border-2 border-slate-50">Some news title</p>
+            <p className=" p-3 border-2 border-slate-100">Some news title</p>
             <p className=" py-2 px-4 bg-gray-100 text-xs text-gray-500">
               21.02.2024
             </p>
+          </div>
+          <div className=" mb-4   shadow-lg text-gray-800">
+            <p className=" p-3 border-2 border-slate-100">Some news title</p>
+            <p className=" py-2 px-4 bg-gray-100 text-xs text-gray-500">
+              21.02.2024
+            </p>
+          </div>
+          <div className=" mb-4   shadow-lg text-gray-800">
+            <p className=" p-3 border-2 border-slate-100">Some news title</p>
+            <p className=" py-2 px-4 bg-gray-100 text-xs text-gray-500">
+              21.02.2024
+            </p>
+          </div>
+          <div className=" text-blue-700 text-center">
+            <a>{"All news >>"}</a>
+          </div>
+          <div id="vk_groups">
+            {VK.Widgets.Group(
+              "vk_groups",
+              { mode: 0, width: "auto", height: "400" },
+              154467839
+            )}
           </div>
         </aside>
       </main>
